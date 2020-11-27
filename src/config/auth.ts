4f714cb05bb64983e2f740df4807536fffe8997f
Devair Restani <devair.juca2013@gmail.com>
2020-11-27 16:46:43 -0300

    retirando informações conjuntas

--- src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    jwt: {
-        secret: "535668dcd3ebefbed12c7bbf9e0adfgdfggfdgdsfgpiaudhfngdfhjkgn948",
-        expiresIn: "1h",
-    },
-};
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,3 @@
 import { createConnection } from "typeorm";
 
 createConnection();
-
-// docker run --name bitbank -e POSTGRES_PASSWORD=bitbank -p 5432:5432 -d postgres
-// yarn typeorm-model-generator -h localhost -d bitbank -u postgres -x bitbank -e postgres -o . -s public
